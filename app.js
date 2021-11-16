f0cac77c90de5a8d0089275dcd01f7f1fee26681
Gaurav <gs926189@gmail.com>
2021-11-16 23:23:49 +0530

    modified design add functionality to add and delete list

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const ejs = require('ejs');
 const _ = require('lodash');
 
 const app = express();
@@ -12,7 +13,10 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static('public'));
 
-mongoose.connect("mongodb+srv://admin-gaurav:gaurav%40sharma@cluster0.udwj8.mongodb.net/toDoListDb?retryWrites=true&w=majority");
+const mongo_url_cloud = "mongodb+srv://admin-gaurav:gaurav%40sharma@cluster0.udwj8.mongodb.net/toDoListDb?retryWrites=true&w=majority";
+const mongo_url_local = "mongodb://localhost:27017/toDoListDB";
+
+mongoose.connect(mongo_url_local);
 
 const itemsSchema = {
   name: String
@@ -41,36 +45,29 @@ const checkFood = new Item({
 
 const defaultItems = [eatFood, buyFood, checkFood];
 
-app.get('/', function(req, res) {
-
-  Item.find({}, function(err, items) {
-    if (err) {
-      console.log(err);
-    } else {
-      if (items.length === 0) {
-        Item.insertMany(defaultItems, function(err, items) {
-          if (err) {
-            console.log(err);
-          }else{
-            res.redirect('/');
-          }
-        });
-
-      } else {
-        res.render('list', {
-          listTitle: "Today",
-          listItems: items
-        });
-      }
-    }
-  });
-
-});
-
 app.get('/favicon.ico', (req, res) => res.status(204));
 
-app.get('/:customList', function(req, res) {
-  const customListName = _.capitalize(req.params.customList);
+app.get('/', function(req, res) {
+  List.find({}, function(err, lists) {
+    if (!err) {
+      if (lists && lists.length > 0) {
+        res.render('home', {
+          haveList: true
+        });
+      } else {
+        res.render('home', {
+          haveList: false
+        });
+      }
+
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+app.get('/lists/:listName', function(req, res) {
+  const customListName = _.capitalize(req.params.listName);
 
   List.findOne({
     name: customListName
@@ -83,16 +80,8 @@ app.get('/:customList', function(req, res) {
           listTitle: foundList.name,
           listItems: foundList.items
         });
-      }else{
-        foundList = new List({
-          name: customListName,
-          items: defaultItems
-        });
-        foundList.save(function(err){
-          if(!err){
-            res.redirect('/' + customListName);
-          }
-        });
+      } else {
+        console.log('list is not saved');
       }
     }
   });
@@ -101,7 +90,65 @@ app.get('/:customList', function(req, res) {
 
 app.get('/about', function(req, res) {
   res.render('about', {});
-})
+});
+
+app.post('/lists/:listName', function(req, res){
+  const listName = req.params.listName;
+  res.redirect('/lists/' + listName);
+});
+
+app.get('/showList', function(req, res){
+  List.find({}, function(err, lists) {
+    if (!err && lists.length > 0) {
+      res.render('list', {
+        listTitle: "Lists",
+        listItems: lists
+      });
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+app.post('/showList', function(req, res) {
+    res.redirect('/showList');
+});
+
+app.post('/addList', function(req, res) {
+  if (req.body.from === 'home') {
+    res.render('list', {
+      listTitle: "Add List",
+      listItems: []
+    });
+  } else if(req.body.from === 'Add List'){
+    const listName = req.body.newListName;
+
+    List.findOne({
+      name: listName
+    }, function(err, foundList) {
+      if (!err) {
+        if (foundList) {
+          console.log("List with this name already exists!");
+        } else {
+          foundList = new List({
+            name: _.capitalize(listName),
+            items: defaultItems
+          });
+          foundList.save(function(err) {
+            if (!err) {
+              res.redirect('/');
+            } else {
+              console.log("while saving error comes " + err);
+            }
+          });
+        }
+      } else {
+        console.log("finding list error " + err);
+      }
+    });
+  }
+
+});
 
 app.post('/', function(req, res) {
   const itemName = req.body.newItem;
@@ -111,22 +158,26 @@ app.post('/', function(req, res) {
     name: itemName
   });
 
-  if(listName === "Today"){
-    item.save(function(err){
-      if(!err){
+  if (listName === "Today") {
+    item.save(function(err) {
+      if (!err) {
         res.redirect('/');
       }
     });
-  }else{
-    List.findOne({name: listName}, function(err, foundList){
-      if(!err){
+  } else {
+    List.findOne({
+      name: listName
+    }, function(err, foundList) {
+      if (!err) {
         foundList.items.push(item);
-        foundList.save(function(err){
-          if(!err){
-            res.redirect('/' + listName);
+        foundList.save(function(err) {
+          if (!err) {
+            res.redirect('/lists/' + listName);
+          }else{
+            console.log('while saving item to list error ' + err);
           }
         });
-      }else{
+      } else {
         console.log(err);
       }
     })
@@ -137,19 +188,37 @@ app.post('/', function(req, res) {
 app.post('/delete', function(req, res) {
   const item = req.body.checkbox;
   const listName = req.body.list;
-  if(listName === "Today"){
+  if (listName === "Today") {
     Item.findByIdAndRemove(req.body.checkbox, function(err) {
       if (err) {
         console.log(err);
       } else {
-        console.log("Successfully removed!!");
+        console.log("Successfully item removed from list " + listName + " !!");
         res.redirect('/');
       }
     });
-  }else{
-    List.findOneAndUpdate({name: listName},{$pull: {items: {_id: item}}},function(err){
-      if(!err){
-        res.redirect('/' + listName);
+  } else if(listName === 'Lists'){
+    // delete list by using it id.
+    List.findByIdAndRemove(req.body.checkbox, function(err){
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Successfully list removed!!");
+        res.redirect('/showList');
+      }
+    })
+  }else {
+    List.findOneAndUpdate({
+      name: listName
+    }, {
+      $pull: {
+        items: {
+          _id: item
+        }
+      }
+    }, function(err) {
+      if (!err) {
+        res.redirect('/lists/' + listName);
       }
     });
   }
